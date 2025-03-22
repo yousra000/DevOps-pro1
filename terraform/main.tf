94850b58c86fdca437019135b9c4e8115732a7b8
yousra000 <yousraramadangad1@gmail.com>
2025-03-22 06:01:25 +0200

    deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,6 +122,6 @@ resource "aws_ecr_repository" "devops_repo" {
 }
 
 # Output instance public IP address
-output "instance_public_ip" {
+output "instance_public_ip"  {
   value     = aws_instance.ec2.public_ip
 }
